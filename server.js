f6e98d20f67b1c2a47bf0d7b54485f99f757656e
venkatesh_Krishnan <Vankatesk_Krishnan@cable.comcast.com>
2018-03-22 14:40:28 +0530

    First Commit

---
+++ server.js
@@ -0,0 +1,56 @@
+const express = require('express');
+const hbs = require('hbs');
+const fs = require('fs');
+
+var app = express();
+
+hbs.registerPartials(__dirname + '/views/partials');
+app.set('view engine','hbs');
+
+app.use((req,res,next)=>{ //acept only function.
+var now = new Date().toString();
+var log = `${now}: ${req.method} ${req.url}`;
+  console.log(log);
+  fs.appendFile('server.log',log + '\n',(err) =>{
+    if (err){
+      console.log('Unable to append in aerver.log');
+    }
+  });
+  next();
+});
+
+// app.use((req,res,next)=>{
+//   res.render('maintainance.hbs');
+// });
+
+app.use(express.static(__dirname + '/public'));
+
+hbs.registerHelper('getCurrentYear',()=>{
+  return new Date().getFullYear();
+});
+
+var text = "welcome"
+hbs.registerHelper('screamIt',(text)=>{
+  return text.toUpperCase();
+});
+
+
+
+app.get('/',(req,res)=>{
+  res.render('home.hbs',{
+    pageTitle : 'Home Page',
+    welcomeMessage : 'Welcome to Home Page'
+  })
+});
+
+app.get('/about',(req,res)=>{
+    res.render('about.hbs',{
+      pageTitle : 'About Page'
+
+    });
+});
+
+app.get('/bad',(req,res)=>{
+  res.send({errorMessage : "Not able to fullfill this request at this time"});
+});
+app.listen(3000);
